1a4a68b43b89d30a2e29b038d6d6f9a1b88a2570
asiddharth <asiddharth@uco.edu>
2021-03-25 08:07:08 -0500

    firebase controller

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,13 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:l3photomemo/model/constant.dart';
+import 'package:l3photomemo/screens/addphotomemo_screen.dart';
 import 'package:l3photomemo/screens/signin_screen.dart';
+import 'package:l3photomemo/screens/userhome_screen.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
   runApp(PhotoMemoApp());
 }
 
@@ -9,9 +15,12 @@ class PhotoMemoApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: Constant.DEV,
       initialRoute: SignInScreen.routeName,
       routes: {
         SignInScreen.routeName: (context) => SignInScreen(),
+        UserHomeScreen.routeName: (context) => UserHomeScreen(),
+        AddPhotoMemoScreen.routeName: (context) => AddPhotoMemoScreen(),
       },
     );
   }
--- lib/screens/signin_screen.dart
+++ lib/screens/signin_screen.dart
@@ -1,4 +1,10 @@
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
+import 'package:l3photomemo/controller/firebasecontroller.dart';
+import 'package:l3photomemo/model/constant.dart';
+import 'package:l3photomemo/screens/userhome_screen.dart';
+
+import 'myview/mydialog.dart';
 
 class SignInScreen extends StatefulWidget {
   static const routeName = '/signInScreen';
@@ -10,6 +16,7 @@ class SignInScreen extends StatefulWidget {
 
 class _SignInState extends State<SignInScreen> {
   _Controller con;
+  GlobalKey<FormState> formkey = GlobalKey<FormState>();
   @override
   void initState() {
     super.initState();
@@ -24,7 +31,37 @@ class _SignInState extends State<SignInScreen> {
       appBar: AppBar(
         title: Text('Sign In'),
       ),
-      body: Text('Sign in'),
+      body: Form(
+          key: formkey,
+          child: SingleChildScrollView(
+            child: Column(
+              children: [
+                TextFormField(
+                  decoration: InputDecoration(
+                    hintText: 'Email',
+                  ),
+                  keyboardType: TextInputType.emailAddress,
+                  autocorrect: false,
+                  validator: con.validateEmail,
+                  onSaved: con.saveEmail,
+                ),
+                TextFormField(
+                  decoration: InputDecoration(
+                    hintText: 'Password',
+                  ),
+                  keyboardType: TextInputType.emailAddress,
+                  obscureText: true,
+                  autocorrect: false,
+                  validator: con.validatePassword,
+                  onSaved: con.savePassword,
+                ),
+                RaisedButton(
+                  onPressed: con.signIn,
+                  child: Text('Sign In', style: Theme.of(context).textTheme.button),
+                )
+              ],
+            ),
+          )),
     );
   }
 }
@@ -32,4 +69,49 @@ class _SignInState extends State<SignInScreen> {
 class _Controller {
   _SignInState state;
   _Controller(this.state);
+  String email;
+  String password;
+
+  String validateEmail(String value) {
+    if (value.contains('@') && value.contains('.'))
+      return null;
+    else
+      return 'invalid email address';
+  }
+
+  void saveEmail(String value) {
+    email = value;
+  }
+
+  String validatePassword(String value) {
+    if (value.length < 6)
+      return 'too short';
+    else
+      return null;
+  }
+
+  void savePassword(String value) {
+    password = value;
+  }
+
+  void signIn() async {
+    if (!state.formkey.currentState.validate()) return;
+    state.formkey.currentState.save();
+    User user;
+    MyDialog.circularProgressStart(state.context);
+    try {
+      user = await FirebaseController.signIn(email, password);
+    } catch (e) {
+      MyDialog.circularProgressStop(state.context);
+      MyDialog.info(
+        context: state.context,
+        title: 'Sign In Error',
+        content: e.toString(),
+      );
+      return;
+    }
+    MyDialog.circularProgressStop(state.context);
+    Navigator.pushNamed(state.context, UserHomeScreen.routeName,
+        arguments: {Constant.ARG_USER: user});
+  }
 }
