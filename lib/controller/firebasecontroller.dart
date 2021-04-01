06542163365cb031881d3a16a1c4ed1649de3132
Hanane Chrif El Asri <hananeca@gmail.com>
2021-04-01 16:22:20 +0100

    sign up

---
+++ lib/controller/firebasecontroller.dart
@@ -0,0 +1,61 @@
+import 'dart:io';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:firebase_storage/firebase_storage.dart';
+import 'package:flutter/material.dart';
+import 'package:photomemo/models/constant.dart';
+
+class FirebaseController {
+  static Future<User> signIn(
+      @required String email, @required String password) async {
+    UserCredential userCredential =
+        await FirebaseAuth.instance.signInWithEmailAndPassword(
+      email: email,
+      password: password,
+    );
+    return userCredential.user;
+  }
+
+  static Future<User> signUp(
+      @required String email, @required String password) async {
+    UserCredential userCredential;
+    try {
+      userCredential = await FirebaseAuth.instance
+          .createUserWithEmailAndPassword(email: email, password: password);
+    } on FirebaseAuthException catch (e) {
+      if (e.code == 'weak-password') {
+        print('The password provided is too weak.');
+      } else if (e.code == 'email-already-in-use') {
+        print('The account already exists for that email.');
+      }
+    } catch (e) {
+      print(e);
+    }
+    return userCredential.user;
+  }
+
+  static Future<void> signOut() async {
+    await FirebaseAuth.instance.signOut();
+  }
+
+  static Future<Map<String, String>> uploadPhotoFile({
+    @required File photo,
+    String filename,
+    @required String uid,
+    @required Function listener,
+  }) async {
+    filename ??= '${Constant.PHOTOIMAGE_FOLDER}/$uid/${DateTime.now()}';
+    UploadTask task = FirebaseStorage.instance.ref(filename).putFile(photo);
+    task.snapshotEvents.listen((TaskSnapshot event) {
+      double progress = event.bytesTransferred / event.totalBytes;
+      if (event.bytesTransferred == event.totalBytes) progress = null;
+      listener(progress);
+    });
+    await task;
+    String downloadURL =
+        await FirebaseStorage.instance.ref(filename).getDownloadURL();
+    return <String, String>{
+      Constant.ARG_DOWNLOADURL: downloadURL,
+      Constant.ARG_FILENAME: filename,
+    };
+  }
+}
